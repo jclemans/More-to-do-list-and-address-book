20a2096212a313da778300874ed248913dfa634b
Jeremy Clemans & Harland Hauger <student@epicodus.com>
2014-03-11 08:57:14 -0700

    add initial project files

---
+++ todo_refactored/lib/list.rb
@@ -0,0 +1,19 @@
+class List
+
+  def initialize(description)
+    @tasks = []
+    @description = description
+  end
+
+  def description
+    @description
+  end
+
+  def add_task (task_to_add)
+    @tasks << task_to_add
+  end
+
+  def show_tasks
+    @tasks
+  end
+end
